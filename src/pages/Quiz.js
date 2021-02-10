b3199f6ff72aaf1a0bfc5c4c16f1a1e8f6d55bd1
faiza203 <fiazfaiza981@gmail.com>
2021-02-10 16:39:41 -0500

    Complete Quiz Style

--- src/pages/Quiz.js
+++ src/pages/Quiz.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import questions from "../json/Question.json";
 import Result from "../components/Result";
-
+import "../css/Quiz.css";
 export default function Quiz() {
   let [questionNo, setQuestionNo] = useState(1);
   let [marks, setMarks] = useState(0);
@@ -13,11 +13,15 @@ export default function Quiz() {
     setQuestionNo((questionNo += 1));
   };
   return questionNo < 8 ? (
-    <div>
-      <h1>#{questionNo}</h1>
-      <p>{questions[questionNo]}</p>
-      <button onClick={addNo}>Yep</button>
-      <button onClick={nextQuestion}>Nope</button>
+    <div id="QuestionDiv">
+      <h1 id="questionNo"># {questionNo}</h1>
+      <p id="question">{questions[questionNo]}</p>
+      <button onClick={addNo} className="takeQuiz">
+        Yep
+      </button>
+      <button onClick={nextQuestion} className="takeQuiz">
+        Nope
+      </button>
     </div>
   ) : (
     <Result marks={marks} />
