4e6c26663aae33c3c9224af124012689deefc670
faiza203 <fiazfaiza981@gmail.com>
2021-02-10 16:47:58 -0500

    Delete yarn.lock

--- src/components/Fail.js
+++ src/components/Fail.js
@@ -1,3 +1,20 @@
+import { Link } from "react-router-dom";
+
 export default function Fail() {
-  return <div>Fail</div>;
+  return (
+    <div>
+      <h1 className="title">NO WORRIES!</h1>
+      <h2>It's ok to not be a developer.</h2>
+      <p>
+        If you do want to be a developer though, hit the button down thataways
+        👇 and check out some awesome free resources at Codecademy.com.
+      </p>
+      <a href="https://codecademy.com/">
+        <button className="takeQuiz">Check out Codecademy 🚀</button>
+      </a>
+      <a href="https://twitter.com/intent/tweet?text=">
+        <button className="takeQuiz">Share on the twitters ✌️️</button>
+      </a>
+    </div>
+  );
 }
--- src/components/Pass.js
+++ src/components/Pass.js
@@ -1,5 +1,9 @@
 export default function Pass() {
-  return <div><h1 className="title">CONGRATS!</h1><h2>Looks like you ARE a real developer!  </h2>
-  <p>Are you a real developer? 💩 yeah.
-</p></div>;
+  return (
+    <div>
+      <h1 className="title">CONGRATS!</h1>
+      <h2>Looks like you ARE a real developer! </h2>
+      <p>Are you a real developer? 💩 yeah.</p>
+    </div>
+  );
 }
