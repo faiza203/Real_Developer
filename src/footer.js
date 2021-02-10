974b4a35af6ea3f7cc34ec4f2913c5efef0062cb
faiza203 <fiazfaiza981@gmail.com>
2021-02-10 08:54:01 -0500

    Create Home.js

---
+++ src/footer.js
@@ -0,0 +1,9 @@
+export default function footer() {
+  return (
+    <div className="footer">
+      <i className="far fa-snowflake" aria-hidden="true"></i>
+      Created By Faiza Fiaz
+      <i className="far fa-snowflake" aria-hidden="true"></i>
+    </div>
+  );
+}
